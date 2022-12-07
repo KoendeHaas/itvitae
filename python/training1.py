87389dd5e48c29168a94442d559aeeb807c0ac31
Koen de Haas <koenhaas@live.nl>
2022-12-07 13:54:29 +0100

    updated training1

--- python/training1.py
+++ python/training1.py
@@ -1,3 +1,3 @@
-print("Hello! What's your name?")
-name = input()
-print(f"Hello {name}!")
+name = input("Hello! What's your name?\n")
+age = input(f"Hello {name}! What's your age?\n")
+print(f"So, your name is {name} and you are {age} years old!")
