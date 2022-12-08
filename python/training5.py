194756caf98e31d13c9368248eafd643a78cb901
Koen de Haas <koenhaas@live.nl>
2022-12-08 12:58:05 +0100

    updated training5

--- python/training5.py
+++ python/training5.py
@@ -3,32 +3,46 @@ contacts = [
     "124-456",
     "piet@email.nl"]
 ]
-action = ""
+
+def list_users():
+    for contact in contacts:
+        print(f"""
+        Name: {contact[0]}
+        Phone: {contact[1]}
+        Email: {contact[2]}
+        """)
+
+def add_user():
+    contact = []
+    contact.append(input("Please enter name: "))
+    contact.append(input("Please enter phone: "))
+    contact.append(input("Please enter email: "))
+    contacts.append(contact)
+ 
+action = None
 
 while action != "exit":
+    # Main menu
     print("""
     What to do?
     1. List entries
     2. Add entry
+    3. Exit
     """)
 
     action = input()
-
-    # List entries
+ 
     match action:
+        # List entries
         case "1":
-            for contact in contacts:
-                print(f"""
-                Name: {contact[0]}
-                Phone: {contact[1]}
-                Email: {contact[2]}
-                """)
+            list_users()
         # Add entry
         case "2":
-            contact = []
-            contact.append(input("Please enter name: "))
-            contact.append(input("Please enter phone: "))
-            contact.append(input("Please enter email: "))
-            contacts.append(contact)
+            add_user()
+        case "3":
+            action = "exit"
+        case _:
+            print("Invalid input")
+            
         # case "exit":
         #     return
