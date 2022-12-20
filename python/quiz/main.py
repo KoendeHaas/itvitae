2e9aa9a14426d4e274434c4795452fd500ba291b
Koen de Haas <koenhaas@live.nl>
2022-12-20 15:06:08 +0100

    added main.py & questions.json

---
+++ python/quiz/main.py
@@ -0,0 +1,22 @@
+import json
+import random
+
+with open ("questions.json") as questions:
+    questions = json.load(questions)
+    random.shuffle(questions)
+
+    print("Welcome to quizerr\n")
+
+    for question in questions:
+        print(f'{question["question"]}\n')
+        print(f'A: {question["A"]}')
+        print(f'B: {question["B"]}')
+        print(f'C: {question["C"]}')
+        print(f'D: {question["D"]}')
+
+        user_input = input("\nAnswer: ").upper()
+
+        if user_input != question["correct"]:
+            print(f"\nIncorrect: the correct answer was: {question['correct']}\n")
+        else:
+            print("\ncorrect!\n")
